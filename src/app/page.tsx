ff6d17f787f1b316770b419cf62a12134fafb638
Erik G <54512095+Zaai90@users.noreply.github.com>
2025-05-19 16:29:45 +0200

    faceplant
    woopsi

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import ContactSection from "@/components/sections/ContactSection";
 import FranchiseSection from "@/components/sections/FranchiseSection";
 import HeroSection from "@/components/sections/HeroSection";
 import OurTeam from "@/components/sections/OurTeam";
+import HowItWorks from "@/components/sections/HowItWorks";
 import Services from "@/components/sections/Services";
-import WhySvevr from "@/components/sections/WhySvevr";
 
 export default function Home() {
   return (
