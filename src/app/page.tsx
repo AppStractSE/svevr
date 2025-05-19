cd422933e3090aa03026c9d330608ce4505077d3
Erik G <54512095+Zaai90@users.noreply.github.com>
2025-05-19 16:27:08 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import ContactSection from "@/components/sections/ContactSection";
 import FranchiseSection from "@/components/sections/FranchiseSection";
 import HeroSection from "@/components/sections/HeroSection";
-import HowItWorks from "@/components/sections/HowItWorks";
 import OurTeam from "@/components/sections/OurTeam";
 import Services from "@/components/sections/Services";
 import WhySvevr from "@/components/sections/WhySvevr";
